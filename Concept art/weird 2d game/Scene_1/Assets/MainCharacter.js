45671344d13dec9900405b3384c7280a651cb890
farreltr <tracey.farrelly@gmail.com>
2014-11-19 09:06:12 +0000

    First commit to blockhead
    Initial commit with the project as is.

---
+++ Concept art/weird 2d game/Scene_1/Assets/MainCharacter.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+
+
+var speed = 0.5;
+var anim: Animator;
+
+
+
+function Start () {
+	anim = GetComponent("Animator");
+}
+
+
+
+
+
+function Update () {
+
+if (Input.GetKey ("a"))
+{
+	//		print ("left arrow key is held down");
+			transform.Translate(Vector2(-10,0) * Time.deltaTime * speed);
+			anim.SetTrigger("walkingLeft");
+
+}
+
+if (Input.GetKey ("d"))
+{
+//			print ("right arrow key is held down");
+			transform.Translate(Vector2(10,0) * Time.deltaTime * speed);
+			anim.SetTrigger("walkingRight");
+			}
+
+//else
+//{
+//		anim.SetTrigger("idle");
+//}
+
+
+
+        if(Input.anyKey == false)
+        {
+        anim.SetTrigger("idle");
+        }
+
+
+
+}
---
+++ Concept art/weird 2d game/Scene_1/Assets/PlayerBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+		private Vector2 direction;
+		private float speed = 1.0f;
+		private bool isWalking = false;
+		private bool walkRight = false;
+		private bool walkLeft = false;
+		private Vector3 endPosition = Vector3.zero;
+		private float duration = 1.0f;
+		private Animator anim;
+
+		void Start ()
+		{
+				anim = this.GetComponent ("Animator") as Animator;
+				print (transform.position);
+		}
+	
+		void Update ()
+		{			
+				if (Input.GetMouseButton (0)) {
+						RaycastHit hit;
+						Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+						if (Physics.Raycast (ray, out hit)) {
+								endPosition = hit.point;
+								//endPosition = transform.position;
+								//endPosition.x = Input.mousePosition.x;
+								print (endPosition);
+								float endX = endPosition.x;
+								isWalking = true;
+								if (endX > transform.position.x) {
+										anim.SetTrigger ("walkingRight");
+								} else {
+										anim.SetTrigger ("walkingLeft");
+								}
+
+								if (isWalking) {
+										transform.position = Vector3.Lerp (transform.position, endPosition, (Time.time * speed) / duration);
+
+								} else {
+										anim.SetTrigger ("idle");
+								}
+						}
+				}
+				if (Mathf.Approximately (transform.position.x, endPosition.x)) {
+						isWalking = false;
+
+				}
+	
+		}
+}	
